a6fef10312126ec903706572b0eeee38ead966f9
PC <lsongulija@gmail.com>
2021-07-11 18:47:07 +0200

    Initial commit

---
+++ Vidly/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the tickets");
+        }
+
+        /**
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        **/
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        //this will be called when we go to Movies/Random
+        //IActionResult is just data that we will return such as JSON 
+        public IActionResult GetMovies()
+        {
+            var movie = new Movie()
+            {
+                Id = 1,
+                Name = "Shrek"
+            };
+            return Ok(movie);
+        }
+    }
+}
