29c76819604d278b03fadb76b4ba416028d9b279
PC <lsongulija@gmail.com>
2021-07-12 16:38:59 +0200

    MoviesController added GET methods for Movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,6 +14,7 @@ namespace Vidly.Controllers
     {
         //this will be called when we go to Movies/Random
         //IActionResult is just data that we will return such as JSON 
+        [HttpGet]
         public IActionResult GetMovies()
         {
             var movie = new Movie()
@@ -22,6 +23,20 @@ namespace Vidly.Controllers
                 Name = "Shrek"
             };
             return Ok(movie);
+        }
+        //HTTP request to api/movies/{id} route. Providing id after movies
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+            return Ok(id);
+
+        }
+
+        //HTTP request to api/movies/{year}/{moth} route, provide these two values
+        [HttpGet("{year}/{month}")]
+        public IActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+"/"+month);
         }
     }
 }
